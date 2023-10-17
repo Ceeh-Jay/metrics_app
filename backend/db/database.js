773e39467769e671a80efa28c50a02de51cba4e6
Chiemeka Elumeziem <chiemekaelumeziem@gmail.com>
2023-10-17 09:32:02 +0100

    refactor database.js

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,9 +1,16 @@
 import sqlite3 from "sqlite3";
+import fs from "fs";
+import path, { __dirname } from "path";
 
-const db = new sqlite3.Database(':memory:');
+const initializeDatabase = () => {
 
+    const db = new sqlite3.Database(":memory:");
 
+    const schemaPath = path.join(__dirname, '../sql_schema/schema.sql');
+    const schema = fs.readFileSync(schemaPath, 'utf-8');
+    db.exec(schema);
 
+    return db;
+};
 
-
-export default db;
+export { initializeDatabase };
---
+++ backend/sql_schema/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS campaigns (
+    name TEXT,
+    impressions INT,
+    clicks INT,
+    conversions INT,
+    spend REAL
+);
